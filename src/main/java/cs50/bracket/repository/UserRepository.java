29291de6e58fe7a081e4ea8d81cea9708408a49d
Juxtaposy <thgawronski@gmail.com>
2023-09-26 12:53:08 +0200

    Create UserRepository interface

---
+++ src/main/java/cs50/bracket/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package cs50.bracket.repository;
+
+import cs50.bracket.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
