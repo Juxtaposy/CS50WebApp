486e720c4e01a46ab82bbb208c3e21e699762c75
Juxtaposy <thgawronski@gmail.com>
2023-09-26 12:31:34 +0200

    Create UserRequest

---
+++ src/main/java/cs50/bracket/controller/request/UserRequest.java
@@ -0,0 +1,21 @@
+package cs50.bracket.controller.request;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public class UserRequest {
+
+    private final String login;
+    private final String password;
+
+    @JsonCreator
+    public UserRequest(String login, String password){
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() { return login; }
+    public boolean checkPassword(String inputPassword) {
+        return inputPassword.equals(this.password);
+    }
+
+}
