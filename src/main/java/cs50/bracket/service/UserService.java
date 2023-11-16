f7c6cfac44a884fb385e48a5179a598232de0c87
Mateusz Gawronski <thgawronski@gmail.com>
2023-11-16 12:26:46 +0100

    Add loginUser method for logging

--- src/main/java/cs50/bracket/service/UserService.java
+++ src/main/java/cs50/bracket/service/UserService.java
@@ -1,13 +1,13 @@
 package cs50.bracket.service;
 
-import cs50.bracket.controller.request.UserRequest;
-import cs50.bracket.controller.response.BracketResponse;
+import cs50.bracket.controller.request.UserLogin;
+import cs50.bracket.controller.request.UserRegister;
 import cs50.bracket.controller.response.UserResponse;
-import cs50.bracket.domain.Bracket;
 import cs50.bracket.domain.User;
-import cs50.bracket.exception.BracketExceptionSupplier;
 import cs50.bracket.exception.UserAlreadyExistsException;
+import cs50.bracket.exception.UserDoesNotExistException;
 import cs50.bracket.exception.UserPasswordsDoNotMatchException;
+import cs50.bracket.exception.UserWrongLoginOrPasswordException;
 import cs50.bracket.mapper.UserMapper;
 import cs50.bracket.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -26,11 +26,11 @@ public class UserService {
         this.userMapper = userMapper;
     }
 
-    public UserResponse create(UserRequest userRequest){
+    public UserResponse registerUser(UserRegister userRegister){
 
-        if (userRepository.findByLogin(userRequest.getLogin()).isEmpty()) {
-            if (userRequest.checkPassword()) {
-                User user = userRepository.save(userMapper.toUser(userRequest));
+        if (userRepository.findByLogin(userRegister.getLogin()).isEmpty()) {
+            if (userRegister.checkPassword()) {
+                User user = userRepository.save(userMapper.toUser(userRegister));
                 return userMapper.toUserResponse(user);
             } else {
                 throw new UserPasswordsDoNotMatchException();
@@ -38,6 +38,19 @@ public class UserService {
         }
         else {
             throw new UserAlreadyExistsException();
+        }
+    }
+
+    public UserResponse loginUser(UserLogin userLogin){
+        Optional<User> user = userRepository.findByLogin(userLogin.getLogin());
+        if (user.isEmpty()){
+            throw new UserDoesNotExistException();
+        }
+        else if (userLogin.getPassword().equals(user.get().getPassword())) {
+            return userMapper.toUserResponse(user.get());
+        }
+        else {
+            throw new UserWrongLoginOrPasswordException();
         }
     }
 }
