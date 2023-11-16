073a9a9fcd0ea0b3edc9e28225c4ce76c0bf47e4
Mateusz Gawronski <thgawronski@gmail.com>
2023-11-16 12:28:23 +0100

    Add CrossOrigin to allow communication with frontend

--- src/main/java/cs50/bracket/controller/BracketApi.java
+++ src/main/java/cs50/bracket/controller/BracketApi.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/brackets")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BracketApi {
 
     private final BracketService bracketService;
